394ce9becb80b9e4b493385c7028177931c8f31f
Thisugithub002 <thisu.jayasooriya109@gmail.com>
2025-09-22 13:38:25 +0530

    Create User Repository

--- app/Repositories/All/User/UserRepository.php
+++ app/Repositories/All/User/UserRepository.php
@@ -3,7 +3,9 @@
 namespace App\Repositories\All\User;
 
 use App\Models\User;
+use App\Repositories\All\User\UserRepositoryInterface;
 use App\Repositories\Base\BaseRepository;
+use Illuminate\Database\Eloquent\Model;
 
 class UserRepository extends BaseRepository implements UserRepositoryInterface
 {
@@ -15,35 +17,10 @@ class UserRepository extends BaseRepository implements UserRepositoryInterface
     /**
      * __construct
      *
-     * @return void
+     * @param User $model
      */
-    public function __construct(
-        User $model,
-    ) {
+    public function __construct(User $model)
+    {
         $this->model = $model;
-    }
-
-    /**
-     * Get user with fitness/member data
-     *
-     * @param int $userId
-     * @return User|null
-     */
-    public function getUserWithFitnessData($userId)
-    {
-        return $this->model->with(['member', 'roles', 'bodyMeasurements' => function ($query) {
-            $query->latest('measurement_date');
-        }])->find($userId);
-    }
-
-    /**
-     * Get user with member relationship
-     *
-     * @param int $userId
-     * @return User|null
-     */
-    public function getUserWithMember($userId)
-    {
-        return $this->model->with(['member', 'roles'])->find($userId);
     }
 }
